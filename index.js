e03f9579c28d1b845c9e17c4dd17befc94fc6d4f
Pwg21 <kcaskade@gmail.com>
2021-02-18 12:15:42 -0700

    finished flow of git inquierer

--- index.js
+++ index.js
@@ -1,89 +1,96 @@
 const { Manager, Engineer, Intern } = require("./src/classes");
 const inquirer = require("inquirer");
 
-function redundant(answer) {
+("use strict");
+
+function redundant(answers) {
   return inquirer.prompt([
     {
       type: "input",
       name: "name",
-      message: `What's the ${answer}'s name?`,
+      message: `What's the ${answers}'s name?`,
     },
     {
       type: "input",
       name: "ID",
-      message: `What's team ${answer}'s ID.`,
+      message: `What's team ${answers}'s ID.`,
     },
     {
       type: "input",
       name: "email",
-      message: `Whats team ${answer}'s email.`,
+      message: `Whats team ${answers}'s email.`,
     },
   ]);
 }
 
 Q1 = () => {
-  redundant("manager");
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "office",
-        message: `What's the team managers office number?`,
-      },
-      {
-        type: "list",
-        name: "type",
-        message: "Add additional Engineer or Inten",
-        choices: ["Engineer", "Intern"],
-      },
-    ])
-    .then(function (answers) {
-      Q2(answers);
-    });
+  redundant("manager").then(() => {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "office",
+          message: `What's the team managers office number?`,
+        },
+        {
+          type: "list",
+          name: "type",
+          message: "Add additional Engineer or Inten",
+          choices: ["Engineer", "Intern"],
+        },
+      ])
+      .then((answers) => {
+        Q2(answers);
+      });
+  });
 };
 
 Q2 = (answers) => {
   switch (answers.type) {
     case "Engineer":
-      redundant("Engineer");
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            name: "Git",
-            message: `What's the Engineers Github?`,
-          },
-          {
-            type: "list",
-            name: "type",
-            message: "Add additional Engineer or Inten",
-            choices: ["Engineer", "Intern"],
-          },
-        ])
-        .then(function (answers) {
-          Q2(answers);
-        });
+      redundant("Engineer").then(() => {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "Git",
+              message: `What's the Engineers Github?`,
+            },
+            {
+              type: "list",
+              name: "type",
+              message:
+                "Finish building team or add additional Engineer or Intern",
+              choices: ["Finished", "Engineer", "Intern"],
+            },
+          ])
+          .then((answers) => {
+            Q2(answers);
+          });
+      });
       break;
 
     case "Intern":
-      redundant("Intern");
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            name: "name",
-            message: `What's school did the intern attend?`,
-          },
-          {
-            type: "list",
-            name: "type",
-            message: "Add additional Engineer or Inten",
-            choices: ["Engineer", "Intern"],
-          },
-        ])
-        .then(function (answers) {
-          Q2(answers);
-        });
+      redundant("Intern").then(() => {
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "name",
+              message: `What's school did the intern attend?`,
+            },
+            {
+              type: "list",
+              name: "type",
+              message:
+                "Finish building team or add additional Engineer or Intern",
+              choices: ["Finished", "Engineer", "Intern"],
+            },
+          ])
+          .then(function (answers) {
+            Q2(answers);
+          });
+      });
       break;
   }
 };
