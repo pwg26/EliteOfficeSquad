d5a4fea510b12de2e8282ff346d80486947efaef
Pwg21 <kcaskade@gmail.com>
2021-02-17 19:28:34 -0700

    refactoring commad line promts

--- index.js
+++ index.js
@@ -1,42 +1,77 @@
 const { Manager, Engineer, Intern } = require("./src/classes");
+const inquirer = require("inquirer");
 
-
-var inquirer = require("inquirer");
-const { switchCase } = require("babel-types");
-
-const Basic = () => {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What's the employees name?",
-        },
-        {
-            type: "input",
-            name: "ID",
-            message: "Whats the emloyees ID",
-        },
-        {
-            type: "input",
-            name: "eamil",
-            message: "Whats the employees email",
-        },
-        {
-            type: "list",
-            name: "type",
-            message: "What type of employee would you like to add?",
-            choices: ["Manager", "Engineer", "Intern"],
-        },
-    ]);
+function redundant(answer) {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: `What's the ${answer}'s name?`,
+    },
+    {
+      type: "input",
+      name: "ID",
+      message: `What's team ${answer}'s ID.`,
+    },
+    {
+      type: "input",
+      name: "email",
+      message: `Whats team ${answer}'s email.`,
+    },
+  ]);
 }
 
-Q1().then(answer => {
-    switch("Manager"){
-        M1()
-    }
+Q1 = () => {
+  redundant("Manager");
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: `What's the team managers office number?`,
+    },
+    {
+      type: "list",
+      name: "type",
+      message: "Add additional Engineer or Inten",
+      choices: ["Engineer", "Intern"],
+    },
+  ]);
+};
 
-  })
+Q2 = () => {
+  switch (answer) {
+    case "Engineer":
+      redundant("Manager");
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "name",
+          message: `What's the Engineers office Github?`,
+        },
+        {
+          type: "list",
+          name: "type",
+          message: "Add additional Engineer or Inten",
+          choices: ["Engineer", "Intern"],
+        },
+      ]);
+      break;
 
-var x = new Manager("Nick", 4355, 345345);
-
-console.log(x.getId());
+    case "Intern":
+      redundant("Manager");
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "name",
+          message: `What's school did the intern attend?`,
+        },
+        {
+          type: "list",
+          name: "type",
+          message: "Add additional Engineer or Inten",
+          choices: ["Engineer", "Intern"],
+        },
+      ]);
+      break;
+  }
+};
