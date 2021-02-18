c4ba08bc6c67c4ae324c5c742144f591a5f01df6
Pwg21 <kcaskade@gmail.com>
2021-02-17 20:27:28 -0700

    having trouple running qnquire prompts seperate from each other

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Manager, Engineer, Intern } = require("./src/classes");
 const inquirer = require("inquirer");
 
 function redundant(answer) {
-  return inquirer.prompt([
+  inquirer.prompt([
     {
       type: "input",
       name: "name",
@@ -22,56 +22,74 @@ function redundant(answer) {
 }
 
 Q1 = () => {
-  redundant("Manager");
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "name",
-      message: `What's the team managers office number?`,
-    },
-    {
-      type: "list",
-      name: "type",
-      message: "Add additional Engineer or Inten",
-      choices: ["Engineer", "Intern"],
-    },
-  ]);
+  //   redundant("manager")
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "office",
+        message: `What's the team managers office number?`,
+      },
+      {
+        type: "list",
+        name: "type",
+        message: "Add additional Engineer or Inten",
+        choices: ["Engineer", "Intern"],
+      },
+    ])
+    .then(function (answers) {
+      Q2(answers);
+    });
 };
 
-Q2 = () => {
-  switch (answer) {
+Q2 = (answers) => {
+  switch (answers.type) {
     case "Engineer":
-      redundant("Manager");
-      inquirer.prompt([
-        {
-          type: "input",
-          name: "name",
-          message: `What's the Engineers office Github?`,
-        },
-        {
-          type: "list",
-          name: "type",
-          message: "Add additional Engineer or Inten",
-          choices: ["Engineer", "Intern"],
-        },
-      ]);
+      redundant("Engineer");
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "Git",
+            message: `What's the Engineers Github?`,
+          },
+          {
+            type: "list",
+            name: "type",
+            message: "Add additional Engineer or Inten",
+            choices: ["Engineer", "Intern"],
+          },
+        ])
+        .then(function (answers) {
+          Q2(answers);
+        });
       break;
 
     case "Intern":
-      redundant("Manager");
-      inquirer.prompt([
-        {
-          type: "input",
-          name: "name",
-          message: `What's school did the intern attend?`,
-        },
-        {
-          type: "list",
-          name: "type",
-          message: "Add additional Engineer or Inten",
-          choices: ["Engineer", "Intern"],
-        },
-      ]);
+      redundant("Intern");
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "name",
+            message: `What's school did the intern attend?`,
+          },
+          {
+            type: "list",
+            name: "type",
+            message: "Add additional Engineer or Inten",
+            choices: ["Engineer", "Intern"],
+          },
+        ])
+        .then(function (answers) {
+          Q2(answers);
+        });
       break;
   }
 };
+
+init = () => {
+  Q1();
+};
+
+init();
